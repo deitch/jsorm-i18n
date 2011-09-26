450ffdbe65c86a3a702582dba23d707755ef9ed6
Avi Deitcher <avi@deitcher.net>
2011-09-26 18:51:54 +0300

    Changed license from multiple to straight Apache 2.0. Let the free usage begin!

--- src/00license.js
+++ src/00license.js
@@ -1,11 +1,23 @@
 /*
- * Copyright © Atomic Inc 2007-2011
+* Copyright © Atomic Inc 2007-2011
  * http://jsorm.com
  *
- * This file contains work that is copyrighted and is distributed under one of several licenses. 
- * You may not use, modify or distribute this work, except under an approved license. 
- * Please visit the Web site listed above to obtain the original work and a license.
+ * This file contains work that is copyrighted and is distributed under the Apache License version 2.0. 
+ * 
+ * Copyright 2007-2011 Atomic Inc
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
  * 
  * Version: @@version@@
  */
-
